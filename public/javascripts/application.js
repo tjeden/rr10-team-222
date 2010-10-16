9dc79461d60a0c61483f6122570e4e83f3350e86
Piotr Barczuk <piotr.barczuk@one99.pl>
2010-10-16 19:32:03 +0200

    hide + reveal 3

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,21 @@
 var block_click = false;
 var elements_to_hide = new Array(2);
+var checked_tile;
 
-function reveal_image_at_index(url, index) {
-  $('#img_'+index).fadeOut(700, function() {
+function reveal_image_at_index(url, index, remove_block) {
+  $('#img_'+index).fadeOut(400, function() {
       $('#img_'+index).html('<img src="' + url + '" alt="revealed tile"/>');
-      $('#img_'+index).fadeIn(700, function() {
-        block_click = false;
+      $('#img_'+index).fadeIn(400, function() {
+        if (remove_block) block_click = false;
       });
   });
 }
 
 function hide_images_at_indexes(index1, index2) {
-  $('#img_'+index1 + ', #img_'+index2).fadeOut(500, function() {
+  $('#img_'+index1 + ', #img_'+index2).fadeOut(200, function() {
     $('#img_'+index1).html('<img src="/images/card_back.jpg" alt="Click to reveal"/>').removeClass('highlighted');
     $('#img_'+index2).html('<img src="/images/card_back.jpg" alt="Click to reveal"/>').removeClass('highlighted');
-    $('#img_'+index1 + ', #img_'+index2).fadeIn(500, function() {
+    $('#img_'+index1 + ', #img_'+index2).fadeIn(200, function() {
         block_click = false;
     });
   });
@@ -22,21 +23,28 @@ function hide_images_at_indexes(index1, index2) {
 
 function prepare_to_hide_images_at_indexes(index1, index2) {
   block_click = setTimeout( 'hide_images_at_indexes(' + index1 + ', ' + index2 + ')', 4000 );
+  checked_tile = null;
   elements_to_hide[0] = index1;
   elements_to_hide[1] = index2;
 }
 
 $(document).ready(function() {
   $('ul.tiles a.hidden_image').click(function() {
-    if (block_click != false) {
-      if (typeof(block_click) == 'number') {
+    var ok_to_continue = true;
+    if (block_click != false)
+    {
+      ok_to_continue = false;
+      if (typeof(block_click) == 'number')
+      {
+        ok_to_continue = true;
         clearTimeout(block_click);
         hide_images_at_indexes(elements_to_hide[0], elements_to_hide[1]);
       }
-      return;
-    }
-    if (typeof(checked_tile) == 'undefined' || checked_tile != $(this).attr('id')) {
+    } 
+    if (ok_to_continue && (typeof(checked_tile) == 'undefined' || checked_tile != $(this).attr('id')))
+    {
       block_click = true;
+      checked_tile = $(this).attr('id');
       elements_to_hide = new Array(2);
       $(this).addClass('highlighted');
 
