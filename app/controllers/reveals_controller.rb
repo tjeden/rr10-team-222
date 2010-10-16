735acdd258a1af3776d06b564ed3134e9157d1f6
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-16 20:22:34 +0200

    games controller

--- app/controllers/reveals_controller.rb
+++ app/controllers/reveals_controller.rb
@@ -1,9 +1,9 @@
 class RevealsController < ApplicationController
   def show
-    game = Game.find(session[:current_game_id])
+    @game = Game.find(session[:current_@game_id])
     @index1 = params[:id].to_i
-    @index2 = game.last_revealed
-    @result = game.reveal_tile(@index1)
-    @image = game.get_photo_from_tile(@index1)
+    @index2 = @game.last_revealed
+    @result = @game.reveal_tile(@index1)
+    @image = @game.get_photo_from_tile(@index1)
   end
 end
