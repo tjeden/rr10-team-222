3735c5697436520f94c534fae7dfe7223af103ca
Piotr Barczuk <piotr.barczuk@one99.pl>
2010-10-16 16:37:36 +0200

    functions for revelaing tiles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,15 @@
-var checked_tiles = new Array();
+var block_click = false;
 
 $(document).ready(function() {
   $('ul.tiles a.hidden_image').click(function() {
-    alert('clicked!');
+    if (typeof(checked_tile) == 'undefined' || checked_tile != $(this).attr('id')) {
+      block_click = true;
+      $(this).addClass('highlighted');
+
+      $.getScript('game/reveal/' + $(this).attr('id').substr(4), function() {
+        alert('Load was performed:' + data);
+        block_click = false;
+      });
+    }
   });
 });
