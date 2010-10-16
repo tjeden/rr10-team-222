302bf4db1d3b7ce063f2d42cab2ce3ff77537cef
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-16 12:31:17 +0200

    a lot of images

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,6 @@ require 'flickr_fu'
 class GamesController < ApplicationController
   def show
     flickr = Flickr.new('config/flickr.yml')
-    photos = flickr.photos.get_recent({})
-    @photo = photos[0]
+    @photos = flickr.photos.get_recent(:pages => 1, :per_page => 25)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     server_id = photo.server
     id = photo.id
     secret = photo.secret
-    image_tag( "http://farm#{farm_id}.static.flickr.com/#{server_id}/#{id}_#{secret}.jpg" )
+    image_tag( "http://farm#{farm_id}.static.flickr.com/#{server_id}/#{id}_#{secret}_s.jpg" )
   end
 end
