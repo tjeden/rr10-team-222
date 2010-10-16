af05a199f9eb8a5cea3ac70aaf83a707e21b0b22
Piotr Barczuk <piotr.barczuk@one99.pl>
2010-10-16 10:15:24 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memoizr::Application.config.secret_token = 'f1109797c5391ba5f6a23da314d853c23603530f0286baf0895210c5c36199680c982fefc6d321b7d3439c75139e959b19df0a9be0a3cb69e8c6b345ee284a7c'
