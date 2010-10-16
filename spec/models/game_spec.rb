e575e5aff174740fd44e4039f26c85b3d0521a00
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-16 19:12:23 +0200

    specs corrected

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -19,10 +19,63 @@ describe Game do
       lambda {
         game = Game.create( :images_category => 'titssdadasdafsdfsafsa')
       }.should change(FlickrImage, :count).by(0)
-      game.errors[:images_category].should eql(['There is not enough photos in that category'])
+      game.errors[:images_category].should eql(['There is not enough photos in this category'])
       Game.count.should eql(0)
       Tile.count.should eql(0)
     end
+  end
 
+  context 'reveal_tile' do
+    before :each do
+      @game = Game.create
+    end
+
+    context 'when it is first click' do
+      it 'sets last_revealed' do
+        @game.reveal_tile(3)
+        @game.reload
+        @game.last_revealed.should eql(3)
+      end
+    end
+
+    context 'when it is last click' do
+      it 'clears last_revealed' do
+        @game.update_attribute(:last_revealed, 5)
+        @game.reveal_tile(3)
+        @game.reload
+        @game.last_revealed.should be_nil
+      end
+    end
+  
+    context 'on second click' do
+      context 'both tiles are the same' do
+        it 'sets both tiles to visible' do
+          image = @game.flickr_images.last
+          tile1 = image.tiles.first
+          tile2 = image.tiles.last
+          @game.update_attribute(:last_revealed, @game.tiles.index(tile1))
+          @game.reveal_tile( @game.tiles.index(tile2))
+          tile1.reload
+          tile2.reload
+          tile1.visible?.should be_true
+          tile2.visible?.should be_true
+        end
+      end
+
+      context 'tiles are different' do
+        it 'sets both tiles to invisible' do
+          @game = Game.create
+          image = @game.flickr_images.last
+          tile1 = image.tiles.first
+          tile2 = @game.flickr_images.first.tiles.last
+          @game.update_attribute(:last_revealed, @game.tiles.index(tile1))
+          @game.reveal_tile( @game.tiles.index(tile2))
+          tile1.reload
+          tile2.reload
+          tile1.visible?.should be_false
+          tile2.visible?.should be_false
+        end
+      end
+    end
   end
 end
