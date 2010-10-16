3a4bcc3e4eed5ffbd42a86d59d83cd8476f41a29
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-16 18:47:04 +0200

    tags corrected in javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,15 +2,15 @@ var block_click = false;
 
 function reveal_image_at_index(url, index) {
   $('#img_'+index).fadeOut(700, function() {
-      $('#img_'+index).html('<img src="' + url + '" alt="revealed tile"');
+      $('#img_'+index).html('<img src="' + url + '" alt="revealed tile"/>');
       $('#img_'+index).fadeIn(700);
   });
 }
 
 function hide_images_at_indexes(index1, index2) {
   $('#img_'+index1 + ', #img_'+index2).fadeOut(500, function() {
-    $('#img_'+index1).html('<img src="/images/card_back.jpg" alt="Click to reveal"');
-    $('#img_'+index2).html('<img src="/images/card_back.jpg" alt="Click to reveal"');
+    $('#img_'+index1).html('<img src="/images/card_back.jpg" alt="Click to reveal"/>');
+    $('#img_'+index2).html('<img src="/images/card_back.jpg" alt="Click to reveal"/>');
     $('#img_'+index1 + ', #img_'+index2).fadeIn(500);
   });
 }
