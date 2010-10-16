f093e6fb5fb8cb6c340308723eefbde93290f7bb
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-16 12:53:14 +0200

    helper with lot of sizes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
 
-  def flickr_img(photo)
+  def flickr_img(photo, options = {})
     farm_id = photo.farm
     server_id = photo.server
     id = photo.id
     secret = photo.secret
-    image_tag( "http://farm#{farm_id}.static.flickr.com/#{server_id}/#{id}_#{secret}_s.jpg" )
+    if options[:size]
+      size = "_t" if options[:size] == :thumbnail
+      size = "_m" if options[:size] == :small
+      size = "" if options[:size] == :medium
+      size = "_b" if options[:size] == :large
+    else
+      size = "_s"
+    end
+    image_tag( "http://farm#{farm_id}.static.flickr.com/#{server_id}/#{id}_#{secret}#{size}.jpg" )
   end
 end
