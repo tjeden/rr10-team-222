0dc02ac3cc3eb6cec23d793f1375a1fbd3ce0a4f
Piotr Barczuk <piotr.barczuk@one99.pl>
2010-10-17 12:37:12 +0200

    image caching v2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,10 @@
 module ApplicationHelper
 
   def flickr_img(photo, options = {})
-    image_tag(flickr_url(photo, options = {}))
+    image_tag(photo.get_url(options))
   end
 
   def flickrify(text)
     "<span class='title'>#{text[0...-1]}</span><span class='title_alt'>#{text[-1..-1]}</span>".html_safe
-  end
-
-  def flickr_url(photo, options = {})
-    farm_id = photo.farm
-    server_id = photo.server
-    id = photo.photo_id
-    secret = photo.secret
-    size = case options[:size]
-    when :thumbnail
-      "_t" 
-    when :small
-      "_m" 
-    when :medium
-      "" 
-    when :large
-      "_b" 
-    else
-      "_s"
-    end
-    "http://farm#{farm_id}.static.flickr.com/#{server_id}/#{id}_#{secret}#{size}.jpg" 
   end
 end
