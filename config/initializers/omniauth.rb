e2898d9df96d174378e42776966f15aacd22fb67
Aleksander Dabrowski <aleks@kumulator.pl>
2010-10-17 12:05:44 +0200

    facebook pai keys corrected

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 #  provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '104273762973020', 'f63a66064030688b79bc57e5fe153e97'
+  if Rails.env == "production"
+    provider :facebook, '168478876501692', '9909b0c54507ab2be976d80ac540e623'
+  else
+    provider :facebook, '104273762973020', 'f63a66064030688b79bc57e5fe153e97'
+  end
 end
